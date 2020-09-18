6f8f1d2a3c5ac439ee3162347e5181412882df04
FSD38\joseph.paolini <joseph.paolini@gm.fsd38.ab.ca>
2020-09-18 11:11:25 -0600

    commet done menu

---
+++ Tanks/Assets/Scripts/Playermanager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermanager : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    SpriteRenderer spriteRen;
+    Animator tankAnim;
+	
+    // Use this for initialization
+	void Start ()
+    {
+		Debug.Log("test:player manger reporting");
+
+        rb2d = GetComponent<Rigidbody2D>();
+        spriteRen = GetComponent<SpriteRenderer>();
+        tankAnim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+        if(Input.GetKey("w") || Input.GetKey("up")) 
+        {
+            rb2d.velocity = new Vector2(0,2);
+            spriteRen.flipY = false;
+            tankAnim.Play("plaermoveA");
+        }
+        else if(Input.GetKey("s") || Input.GetKey("down"))
+        {
+            rb2d.velocity = new Vector2(0,-2);
+            spriteRen.flipY = true;
+            tankAnim.Play("plaermoveA");
+        }
+        else if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            rb2d.velocity = new Vector2(2,0);
+            spriteRen.flipX = false;
+            tankAnim.Play("playermoveB");
+        }
+        else if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            rb2d.velocity = new Vector2(-2,0);
+            spriteRen.flipX = true;
+            tankAnim.Play("playermoveB");
+        }
+        else
+        {
+            rb2d.velocity = new Vector2(0, 0);
+        }
+
+    }
+
+}
---
+++ Tanks/Assets/Scripts/titlepagemanager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class titlepagemanager : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject tittlepage;
+    public GameObject cursor;
+    public GameObject[] curserposition;
+    public GameObject playerSelectIndicator;
+    public Text thisText;
+    public GameObject levelone;
+
+    private bool ongamestartselection;
+
+    // Use this for initialization
+    void Start ()
+    {
+        ongamestartselection = false;
+        thisText = playerSelectIndicator.GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+
+        if (Input.GetKeyDown("up") || Input.GetKeyDown("down"))
+        {
+           if(ongamestartselection)
+            {
+                ongamestartselection = false;
+            }
+           else
+            {
+              ongamestartselection = true;
+            }
+        }
+        if(ongamestartselection)
+        {
+            cursor.transform.position = curserposition[1].transform.position;
+        }
+        else
+        {
+            cursor.transform.position = curserposition[0].transform.position;
+        }
+
+        if (Input.GetKeyDown("space") || Input.GetKeyDown("return"))
+        {
+            if (ongamestartselection == true)
+            {
+                
+                player.SetActive(true);
+                levelone.SetActive(true);
+                tittlepage.SetActive(false);
+
+            }
+            if (ongamestartselection == false)
+            {
+                if (thisText.text == "1")
+                {
+                    thisText.text = "2";
+                }
+                else
+                {
+                    thisText.text = "1";
+                }
+
+            }
+
+        }
+        
+            
+        
+        
+    }
+
+}
